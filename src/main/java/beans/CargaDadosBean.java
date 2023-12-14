e98d188427dc87636d3dad864201479aedf99cf7
welin <welin@Pipeline>
2023-12-14 20:25:46 -0300

    Processos Seletivos - Versão 3.3 - Versão final

--- src/main/java/beans/CargaDadosBean.java
+++ src/main/java/beans/CargaDadosBean.java
@@ -46,53 +46,53 @@ public class CargaDadosBean
     }
 
     private void carregarDados() {
-        Usuario usuario1 = cadastrarUsuario("Nome1", "email1@example.com", "senha1", Perfil.CANDIDATO, LocalDate.of(1990, 1, 1), 123456789L);
-        Usuario usuario2 = cadastrarUsuario("Nome2", "email2@example.com", "senha2", Perfil.ADMINISTRADOR, LocalDate.of(1995, 5, 5), 987654321L);
-        Usuario usuario3 = cadastrarUsuario("Nome3", "email3@example.com", "senha3", Perfil.EDITOR, LocalDate.of(1999, 10, 5), 987654321L);
+        Usuario usuario1 = cadastrarUsuario("Emilly", "candidato@gmail.com", "1234", Perfil.CANDIDATO, LocalDate.of(1990, 1, 1), 123456789L);
+        Usuario usuario2 = cadastrarUsuario("Caua", "admin@gmail.com", "1234", Perfil.ADMINISTRADOR, LocalDate.of(1995, 5, 5), 987654321L);
+        Usuario usuario3 = cadastrarUsuario("Junio", "editor@gmail.com", "1234", Perfil.EDITOR, LocalDate.of(1999, 10, 5), 987654321L);
         
         // Criação de Processo Seletivo
         ProcessoSeletivo processo1 = new ProcessoSeletivo(
                 LocalDate.of(2023, 1, 1),
                 LocalDate.of(2023, 2, 1),
-                "Processo 1",
+                "Processo Seletivo Ensino Médio 2023",
                 true,
                 usuario2,
                 LocalDate.of(2023, 1, 15),
                 LocalDate.of(2023, 2, 1),
                 LocalDate.of(2023, 2, 15),
-                "Edital 1",
+                "ifnmg.com/processo1/edital3",
                 null // membros serão adicionados posteriormente
         );
         ProcessoSeletivo processo2 = new ProcessoSeletivo(
                 LocalDate.of(2023, 3, 1),
                 LocalDate.of(2023, 4, 1),
-                "Processo 2",
+                "Processo Seletivo Superior 2023",
                 true,
                 usuario2,
                 LocalDate.of(2023, 3, 15),
                 LocalDate.of(2023, 4, 1),
                 LocalDate.of(2023, 4, 15),
-                "Edital 2",
+                "ifnmg.com/processo5/edital2",
                 null // membros serão adicionados posteriormente
         );
         
         ProcessoSeletivo processo3 = new ProcessoSeletivo(
                 LocalDate.of(2023, 4, 4),
                 LocalDate.of(2023, 5, 6),
-                "Processo 3",
+                "Processo Seletivo Integrado 2023",
                 true,
                 usuario2,
                 LocalDate.of(2023, 1, 15),
                 LocalDate.of(2023, 2, 1),
                 LocalDate.of(2023, 2, 15),
-                "Edital 3",
+                "ifnmg.com/processo8/edital7",
                 null // membros serão adicionados posteriormente
         );
         Postagem postagem = new Postagem(
-            "Postagem1",
+            "Título: Pesquisadores Anunciam Descoberta de Nova Espécie Marinha no Fundo do Oceano",
             LocalDate.now(),
             usuario3,
-            "Conteúdo da Postagem",
+            "Numa expedição submarina recente, cientistas fizeram uma descoberta fascinante no fundo do oceano: uma nova espécie de vida marinha nunca antes catalogada. Os pesquisadores, pertencentes a uma equipe internacional de biólogos marinhos, encontraram a espécie a uma profundidade recorde de 3.000 metros.",
             null,
             Publicacao.NOTICIA
     );
@@ -100,6 +100,7 @@ public class CargaDadosBean
         entityManager.persist(processo1);
         entityManager.persist(processo2);
         entityManager.persist(processo3);
+ 
  
         // Criação de Postagens
         Participa participa1 = new Participa(processo1, usuario1, LocalDate.now(), 100.0f);
@@ -113,19 +114,19 @@ public class CargaDadosBean
 
         // Criação de Postagens
         Postagem postagem1 = new Postagem(
-                "Título da Postagem 1",
+                "IFNMG Anuncia Novo Edital de Processo Seletivo!",
                 LocalDate.of(2023, 1, 10),
                 usuario3,
-                "Conteúdo da Postagem 1",
+                "O Instituto Federal do Norte de Minas Gerais (IFNMG) surpreendeu a comunidade acadêmica ao lançar oficialmente um novo edital de processo seletivo. Com oportunidades diversas, o edital promete movimentar o cenário educacional na região. Estudantes e profissionais já estão ansiosos para concorrer às vagas disponíveis.",
                 null,
                 Publicacao.EDITAL
         );
 
         Postagem postagem2 = new Postagem(
-                "Título da Postagem 2",
+                "Edital Fresquinho: IFNMG Divulga Processo Seletivo",
                 LocalDate.of(2023, 2, 15),
                 usuario3,
-                "Conteúdo da Postagem 2",
+                "Grandes expectativas cercam o lançamento do mais recente edital do Instituto Federal do Norte de Minas Gerais (IFNMG). Com oportunidades para diversas áreas, o edital abre portas para novos talentos e promete impulsionar a educação na região. Os interessados já podem começar os preparativos para concorrer às vagas.",
                 null,
                 Publicacao.EDITAL
         );
